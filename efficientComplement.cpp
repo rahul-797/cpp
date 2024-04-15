2952e44d1bf97a9b6593cfcef55cac04b83bec90
rahul-797 <rahulsangwan797@gmail.com>
2024-04-15 19:57:36 +0530

    complements efficiently

--- efficientComplement.cpp
+++ efficientComplement.cpp
@@ -3,18 +3,15 @@
 using namespace std;
 
 int main() {
-  int input, mask = 0, i = 0;
+  int input, mask = 0;
   cin >> input;
   int n = input;
 
   while (n != 0) {
-    n = n >> 1;
-    i++;
-  }
-  while (i > 0) {
     mask = (mask << 1) | 1;
-    i--;
+    n = n >> 1;
   }
+
   cout << ((~input) & mask) << endl;
 
   return 0;
