8d555d6fef450e57687e972772c0d094da043d4b
rahul-797 <rahulsangwan797@gmail.com>
2024-04-17 21:19:27 +0530

    sorts triplet

--- sort012.cpp
+++ sort012.cpp
@@ -3,13 +3,23 @@ using namespace std;
 
 void arrange(int arr[], int len) {
   int i = 0;
-  int j = 0;
+  int j = 1;
 
   while (i < len) {
     if (arr[i] == 1) {
+      j = i + 1;
+      while (j > i && j < len) {
+        if (arr[j] == 0) {
+          swap(arr[i], arr[j]);
+          arrange(arr, len);
+        }
+        j++;
+      }
+    }
+    if (arr[i] == 2) {
       int length = len;
       while (length > i) {
-        if (arr[length] == 0) {
+        if (arr[length] == 1 || arr[length] == 0) {
           swap(arr[i], arr[length]);
         }
         length--;
@@ -20,11 +30,11 @@ void arrange(int arr[], int len) {
 }
 
 int main() {
-  int arr[10] = {0, 0, 0, 0, 1, 1, 1, 1, 0, 0};
+  int arr[15] = {2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
   int len = sizeof(arr) / sizeof(arr[0]);
   arrange(arr, len);
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < len; i++) {
     cout << arr[i] << " ";
   }
   cout << endl;
