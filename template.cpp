e8bb44864835ac9bc9de2d5384481c685490ee12
rahul-797 <rahulsangwan797@gmail.com>
2024-04-21 21:00:01 +0530

    cooking ninja problem using BS

--- template.cpp
+++ template.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 
 int main() {
-  int n, req;
-  cin >> n >> req;
-  int arr[n];
-  for (int i = 0; i < n; i++) {
-    cin >> arr[i];
-  }
-  for (int i = 0; i < n; i++) {
-    cout << arr[i];
-  }
+  int n;
   return 0;
 }
