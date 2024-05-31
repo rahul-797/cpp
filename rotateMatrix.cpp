e2eee3ea4bb84e9ebc9798aaa5bd0a3cd126125d
rahul-797 <rahulsangwan797@gmail.com>
2024-05-31 18:26:29 +0530

    initial commit

--- rotateMatrix.cpp
+++ rotateMatrix.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 
 int n = 4;
+int newPos[2];
+int buffer = 0;
+int startPos[2];
 
-bool switched(unordered_map<int, int> hash, int row, int col) {
-  for (auto [i, j] : hash) {
-    if (i == row && j == col) return true;
+bool switched(unordered_map<int, int>::iterator start,
+              unordered_map<int, int>::iterator end, int row, int col) {
+  for (auto i = start; i != end; i++) {
+    if (i->first == row && i->second == col) return true;
   }
-  // simply find the pair in hash using builtin fn. remove this method(fn)
   return false;
 }
 
-int* correctPos(int* pos) {
-  int temp = pos[1];
-  pos[1] = n + 1 - pos[0];
-  pos[0] = temp;
-  return pos;
+int* correctPos(int pos[]) {
+  newPos[0] = pos[1];
+  newPos[1] = n + 1 - pos[0];
+  return &newPos[0];
 }
 
-void changePos(int* arr[][n]) {}
+void changePos(vector<vector<int>>& matrix, int pos[], int newPos[]) {
+  buffer = matrix[newPos[0]][newPos[1]];
+  matrix[newPos[0]][newPos[1]] = matrix[pos[0]][pos[1]];
+}
 
 int main() {
-  int matrix[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+  vector<vector<int>> matrix = {
+      {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
   unordered_map<int, int> hash;
 
-  for (int row = 0; row < n; row++) {
-    for (int col = 0; col < n; col++) {
-      if (switched(hash, row, col)) continue;
+  for (int row = 1; row <= n; row++) {
+    for (int col = 1; col <= n; col++) {
+      if (switched(hash.begin(), hash.end(), row, col)) continue;
+      int pos[2] = {row, col};
+      correctPos(pos);
+      startPos[0] = row, startPos[1] = col;
     }
   }
 
