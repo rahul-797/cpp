e48a533a32044d80e9d6101bbba550e5e2252119
rahul-797 <rahulsangwan797@gmail.com>
2024-06-07 23:12:08 +0530

    optimised bin search

--- recursionArray.cpp
+++ recursionArray.cpp
@@ -31,28 +31,20 @@ bool linearSearch(int *arr, int size, int key) {
   return linearSearch(arr + 1, size - 1, key);
 }
 
-bool binarySearch(int *arr, int size, int key) {
-  if (size == 0) {
-    cout << "Not found" << endl;
-    return false;
+int binarySearch(int *arr, int key, int start, int end) {
+  if (start > end) {
+    return -1;
   }
-  int s = 0;
-  int e = size - 1;
-  int mid = s + (e - s) / 2;
-  if (key == arr[mid]) {
-    cout << "Found" << endl;
-    return true;
-  }
-  if (key < arr[mid]) {
-    int newSize = size & 1 ? (size / 2) : (size / 2) - 1;
-    return binarySearch(arr, newSize, key);
-  } else {
-    return binarySearch(arr + mid + 1, (size / 2), key);
-  }
+  int mid = start + (end - start) / 2;
+  if (key == arr[mid]) return mid;
+  if (key < arr[mid])
+    return binarySearch(arr, key, start, mid - 1);
+  else
+    return binarySearch(arr, key, mid + 1, end);
 }
 
 int main() {
-  int arr[6] = {1, 2, 3, 4, 5, 4};
+  int arr[6] = {1, 2, 3, 4, 5, 6};
   int size = sizeof(arr) / sizeof(arr[0]);
   for (auto i : arr) {
     cout << i << " ";
@@ -68,6 +60,7 @@ int main() {
   int key2;
   cin >> key2;
   sort(arr, arr + size);
-  binarySearch(arr, size, key2);
+  int index = binarySearch(arr, key2, 0, size - 1);
+  cout << "Found at index: " << index << endl;
   return 0;
 }
