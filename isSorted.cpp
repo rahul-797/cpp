51a9abab788b267b2f5160d530933678b5d18bb8
rahul-797 <rahulsangwan797@gmail.com>
2024-06-07 21:49:15 +0530

    finds sum too using recursion

--- isSorted.cpp
+++ isSorted.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int i = 0;
+int sum(int *arr, int size) {
+  if (size == 0) return 0;
+  return arr[0] + sum(arr + 1, size - 1);
+}
 
 bool isSorted(int *arr, int size) {
-  if (i + 1 < size && arr[i] > arr[i + 1]) {
-    cout << "Not sorted" << endl;
-    return false;
-  }
-  if (i == size - 1) {
+  if (size == 0 || size == 1) {
     cout << "Sorted" << endl;
     return true;
   }
-  i++;
-  return isSorted(arr, size);
+  if (arr[0] > arr[1]) {
+    cout << "Not sorted" << endl;
+    return false;
+  }
+  return isSorted(arr + 1, size - 1);
 }
 
 int main() {
-  int arr[6] = {1, 2, 3, 4, 5, 6};
+  int arr[6] = {1, 2, 3, 4, 5, 4};
   int size = sizeof(arr) / sizeof(arr[0]);
   for (auto i : arr) {
     cout << i << " ";
   }
   cout << endl;
   isSorted(arr, size);
+  cout << "Sum of elements is: " << sum(arr, size) << endl;
   return 0;
 }
