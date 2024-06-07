91dcfa30209ebe73fbc36f5f24497add320b8626
rahul-797 <rahulsangwan797@gmail.com>
2024-06-07 22:58:00 +0530

    various operations using recursion

--- recursionArray.cpp
+++ recursionArray.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
@@ -30,6 +31,26 @@ bool linearSearch(int *arr, int size, int key) {
   return linearSearch(arr + 1, size - 1, key);
 }
 
+bool binarySearch(int *arr, int size, int key) {
+  if (size == 0) {
+    cout << "Not found" << endl;
+    return false;
+  }
+  int s = 0;
+  int e = size - 1;
+  int mid = s + (e - s) / 2;
+  if (key == arr[mid]) {
+    cout << "Found" << endl;
+    return true;
+  }
+  if (key < arr[mid]) {
+    int newSize = size & 1 ? (size / 2) : (size / 2) - 1;
+    return binarySearch(arr, newSize, key);
+  } else {
+    return binarySearch(arr + mid + 1, (size / 2), key);
+  }
+}
+
 int main() {
   int arr[6] = {1, 2, 3, 4, 5, 4};
   int size = sizeof(arr) / sizeof(arr[0]);
@@ -39,9 +60,14 @@ int main() {
   cout << endl;
   isSorted(arr, size);
   cout << "Sum of elements is: " << sum(arr, size) << endl;
-  cout << "Enter element to search: ";
+  cout << "Enter element to search(linear search): ";
   int key;
   cin >> key;
   linearSearch(arr, size, key);
+  cout << "Enter element to search(binary search): ";
+  int key2;
+  cin >> key2;
+  sort(arr, arr + size);
+  binarySearch(arr, size, key2);
   return 0;
 }
