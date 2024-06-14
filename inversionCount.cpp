aea50b31829db27ae3e120393dc84739ce6f14fa
rahul-797 <rahulsangwan797@gmail.com>
2024-06-14 10:48:15 +0530

    inversion count using merge sort

--- inversionCount.cpp
+++ inversionCount.cpp
@@ -1,50 +1,43 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int mergeArr(int *arr, int s, int e, int mid) {
   int inv = 0;
-  int len1 = mid - s + 1;
-  int len2 = e - mid;
-  int *arr1 = new int[len1];
-  int *arr2 = new int[len2];
+  vector<int> vec;
+  int left = s;
+  int right = mid + 1;
 
-  int k = s;
-  for (int i = 0; i < len1; i++) {
-    arr1[i] = arr[k++];
-  }
-  k = mid + 1;
-  for (int j = 0; j < len2; j++) {
-    arr2[j] = arr[k++];
-  }
-
-  int i = 0, j = 0;
   int index = s;
-  while (i < len1 && j < len2) {
-    if (arr1[i] <= arr2[j]) {
-      arr[index++] = arr1[i++];
+  while (left <= mid && right <= e) {
+    if (arr[left] <= arr[right]) {
+      vec.push_back(arr[left++]);
     } else {
-      arr[index++] = arr2[j++];
-      inv += len1 - i;
+      vec.push_back(arr[right++]);
+      inv += mid - left + 1;
     }
   }
-  while (i < len1) {
-    arr[index++] = arr1[i++];
+  while (left <= mid) {
+    vec.push_back(arr[left++]);
   }
-  while (j < len2) {
-    arr[index++] = arr2[j++];
+  while (right <= e) {
+    vec.push_back(arr[right++]);
+  }
+  for (int i = s; i <= e; i++) {
+    arr[i] = vec[i - s];
   }
 
-  delete[] arr1;
-  delete[] arr2;
   return inv;
 }
 
 int mergeSort(int *arr, int s, int e) {
-  if (s >= e) return 0;
+  int inv = 0;
+  if (s >= e) return inv;
   int mid = s + (e - s) / 2;
-  mergeSort(arr, s, mid);
-  mergeSort(arr, mid + 1, e);
-  return mergeArr(arr, s, e, mid);
+  inv += mergeSort(arr, s, mid);
+  inv += mergeSort(arr, mid + 1, e);
+  inv += mergeArr(arr, s, e, mid);
+  return inv;
 }
 
 int main() {
@@ -56,6 +49,10 @@ int main() {
   cout << endl;
 
   int inv = mergeSort(arr, 0, sizeof(arr) / sizeof(arr[0]) - 1);
+  for (auto i : arr) {
+    cout << i << " ";
+  }
+  cout << endl;
   cout << "Inversion: " << inv << endl;
   return 0;
 }
