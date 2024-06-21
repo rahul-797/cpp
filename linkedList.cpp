661f593e846143c004ad75dbd013a79acc529eb5
rahul-797 <rahulsangwan797@gmail.com>
2024-06-21 14:07:58 +0530

    added operations

--- linkedList.cpp
+++ linkedList.cpp
@@ -27,6 +27,51 @@ void insertEnd(Node*& head, int val) {
   temp->next = node;
 }
 
+void insertAtPos(Node*& head, int val, int pos) {
+  Node* node = new Node(val);
+  Node* temp = head;
+  if (pos == 1) {
+    node->next = head;
+    head = node;
+    return;
+  }
+  while ((pos--) - 2) {
+    temp = temp->next;
+  }
+  node->next = temp->next;
+  temp->next = node;
+}
+
+void deleteBegin(Node*& head) {
+  Node* temp = head->next;
+  delete head;
+  head = temp;
+}
+
+void deleteEnd(Node*& head) {
+  Node* temp = head;
+  while (temp->next->next != NULL) {
+    temp = temp->next;
+  }
+  delete temp->next;
+  temp->next = NULL;
+}
+
+void deleteAtPos(Node*& head, int pos) {
+  Node* temp = head;
+  if (pos == 1) {
+    head = head->next;
+    delete temp;
+    return;
+  }
+  while ((pos--) - 2) {
+    temp = temp->next;
+  }
+  Node* temp2 = temp->next->next;
+  delete temp->next;
+  temp->next = temp2;
+}
+
 void print(Node*& head) {
   Node* temp = head;
   while (temp != NULL) {
@@ -40,9 +85,21 @@ int main() {
   Node* head = new Node(5);
   insertBegin(head, 4);
   insertBegin(head, 3);
-  insertEnd(head, 6);
   insertEnd(head, 7);
+  insertEnd(head, 8);
+  print(head);
 
+  insertAtPos(head, 6, 4);
+  insertAtPos(head, 1, 1);
+  insertAtPos(head, 2, 2);
+  insertAtPos(head, 9, 9);
+  print(head);
+
+  deleteBegin(head);
+  deleteEnd(head);
+  print(head);
+
+  deleteAtPos(head, 1);
   print(head);
 
   return 0;
