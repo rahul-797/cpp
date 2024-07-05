2e09062de5a7f012d3126059612add7f66385092
rahul-797 <rahulsangwan797@gmail.com>
2024-07-05 22:44:32 +0530

    initial commit

---
+++ redundantBrackets.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void print(stack<char>& s) {
+  if (s.empty()) return;
+  char last = s.top();
+  s.pop();
+  print(s);
+  s.push(last);
+  cout << s.top();
+}
+
+void hasRedundantBracket(stack<char>& s) {
+  cout << s.top() << endl;
+  if (s.empty()) {
+    cout << "No redundant brackets" << endl;
+    return;
+  }
+  char removed = s.top();
+  s.pop();
+  char top = s.top();
+  if (top == removed && top == ')') {
+    cout << "Has redundant brackets" << endl;
+  }
+  hasRedundantBracket(s);
+  s.push(top);
+}
+
+int main() {
+  stack<char> s;
+  s.push('(');
+  s.push('a');
+  s.push(')');
+
+  print(s);
+  cout << endl;
+  hasRedundantBracket(s);
+  return 0;
+}
