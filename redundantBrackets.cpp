54d6bb676801fddf5ad81a433fb581f6dc51850f
rahul-797 <rahulsangwan797@gmail.com>
2024-07-07 10:27:59 +0530

    Works now

--- redundantBrackets.cpp
+++ redundantBrackets.cpp
@@ -11,36 +11,45 @@ void print(stack<char>& s) {
   cout << s.top();
 }
 
-void hasRedundantBracket(stack<char>& s) {
+void hasRedundantBracket(stack<char>& s, bool* flag, int count = 0) {
+  if (count == 2) {
+    *flag = true;
+    return;
+  }
   if (s.empty()) {
-    cout << "No redundant brackets" << endl;
     return;
   }
   char removed = s.top();
   s.pop();
   if (s.empty()) {
-    cout << "No redundant brackets" << endl;
     return;
   }
   char top = s.top();
-  if (top == removed && top == ')') {
-    cout << "Has redundant brackets" << endl;
-    return;
+  if (top == removed && (top == ')' || top == '(')) {
+    count++;
   }
-  hasRedundantBracket(s);
+  hasRedundantBracket(s, flag, count);
   s.push(top);
 }
 
 int main() {
   stack<char> s;
+  bool* flag = new bool(false);
   s.push('(');
+  s.push('b');
+  s.push('+');
   s.push('(');
+  s.push('b');
+  s.push('+');
   s.push('a');
   s.push(')');
   s.push(')');
 
   print(s);
   cout << endl;
-  hasRedundantBracket(s);
+  hasRedundantBracket(s, flag, 0);
+  cout << "Has redundant brackets: ";
+  (*flag) ? cout << "true" : cout << "false";
+  cout << endl;
   return 0;
 }
