cf829ea0b084acbc57b8b2d2340f0f82b8478361
rahul-797 <rahulsangwan797@gmail.com>
2024-07-27 16:13:58 +0530

    updated

--- balanceBST.cpp
+++ balanceBST.cpp
@@ -50,6 +50,9 @@ void print(Node* node) {
 Node* convert(int* arr, int start, int end) {
   if (start > end) return NULL;
   int mid = (start + end) / 2;
+  if (start == 0 && end == 6) {
+    mid = 0;
+  }
   Node* root = new Node(arr[mid]);
   root->left = convert(arr, start, mid - 1);
   root->right = convert(arr, mid + 1, end);
