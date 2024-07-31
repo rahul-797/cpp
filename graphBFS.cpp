fa02d5cf42e6ac472ebf106d91a700c7b510863e
rahul-797 <rahulsangwan797@gmail.com>
2024-07-31 13:58:20 +0530

    improved

--- graphBFS.cpp
+++ graphBFS.cpp
@@ -4,7 +4,28 @@
 #include <vector>
 using namespace std;
 
-void addEdge(vector<int> adj[], int src, int dest) { adj[src].push_back(dest); }
+void addEdge(vector<int> adj[], int src, int dest) {
+  for (int i = 0; i < adj[src].size(); i++) {
+    if (adj[src][i] == dest) return;
+  }
+  adj[src].push_back(dest);
+}
+
+void printAdjacencyList(vector<int> adj[]) {
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < adj[i].size(); j++) {
+      cout << i << " -> " << adj[i][j] << endl;
+    }
+  }
+}
+
+void uniToBiDirectional(vector<int> adj[]) {
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < adj[i].size(); j++) {
+      addEdge(adj, adj[i][j], i);
+    }
+  }
+}
 
 bool addVisited(vector<int>& visited, int val) {
   if (find(visited.begin(), visited.end(), val) != visited.end()) {
@@ -15,36 +36,37 @@ bool addVisited(vector<int>& visited, int val) {
   }
 }
 
+void print_queue(deque<int> q) {
+  while (!q.empty()) {
+    cout << q.front() << " ";
+    q.pop_front();
+  }
+  cout << endl;
+}
+
 void traverseGraph(vector<int> adj[], deque<int>& q, vector<int>& visited) {
   if (q.empty()) return;
   int front = q.front();
   addVisited(visited, front);
   q.pop_front();
-  for (int i = front; i < adj[front].size(); i++) {
-    if (find(q.begin(), q.end(), adj[front][i]) == q.end())
+  for (int i = 0; i < adj[front].size(); i++) {
+    if (find(visited.begin(), visited.end(), adj[front][i]) == visited.end()) {
       q.push_back(adj[front][i]);
+    }
   }
   return traverseGraph(adj, q, visited);
 }
 
 int main() {
   vector<int> adj[6];
-  addEdge(adj, 0, 1);
-  addEdge(adj, 0, 2);
-  addEdge(adj, 1, 0);
-  addEdge(adj, 1, 2);
-  addEdge(adj, 1, 3);
   addEdge(adj, 2, 0);
-  addEdge(adj, 2, 1);
-  addEdge(adj, 2, 4);
-  addEdge(adj, 3, 1);
-  addEdge(adj, 3, 4);
-  addEdge(adj, 4, 2);
-  addEdge(adj, 4, 3);
+  addEdge(adj, 0, 1);
+  addEdge(adj, 2, 3);
 
   vector<int> visited;
   deque<int> q;
   q.push_back(0);
+  uniToBiDirectional(adj);
   traverseGraph(adj, q, visited);
 
   for (int i : visited) {
