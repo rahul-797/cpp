185eb26efb3591ef0134a230b6a8874ec706544c
rahul-797 <rahulsangwan797@gmail.com>
2024-08-14 06:23:43 +0530

    longest subarray with given sum

---
+++ subArrSum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+  int arr[37] = {0, 2, 5, 3, 3, 4, 4, 3, 0, 5, 5, 4, 4, 4, 3, 2, 0, 2, 3,
+                 1, 3, 0, 4, 3, 1, 4, 5, 2, 4, 3, 1, 4, 5, 0, 3, 4, 0};
+  int k = 52;
+  int len = sizeof(arr) / sizeof(arr[0]);
+  int counter = 0;
+  int currSum = 0;
+  int max = 0;
+
+  queue<int> q;
+  while (counter < len) {
+    if (currSum == k) {
+      if (q.size() > max) max = q.size();
+      if (arr[counter] == 0) {
+        q.push(arr[counter]);
+        counter++;
+        continue;
+      }
+      currSum -= q.front();
+      q.pop();
+    }
+    if (currSum < k) {
+      q.push(arr[counter]);
+      currSum += arr[counter];
+      counter++;
+    }
+    if (currSum > k) {
+      currSum -= q.front();
+      q.pop();
+    }
+  }
+
+  cout << "Max subarray length: " << max << endl;
+
+  return 0;
+}
