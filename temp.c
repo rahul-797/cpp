dd25abc63df51b8271d9b5920e7ba9565958db16
rahul-797 <rahulsangwan797@gmail.com>
2024-10-24 17:01:33 +0530

    tests

--- temp.c
+++ temp.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
   int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
   int(*p)[3];
   p = &arr[0];
-  printf("%p\n", *p);
-  printf("%p\n", *(p + 1));
+
+  int *i = NULL;
+  i = (int *)malloc(sizeof(int));
+  i = NULL;
+  printf("%p", i);
   return 0;
 }
