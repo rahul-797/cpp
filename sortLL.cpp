df59f65dbeab0967e50a6d0661397c774e986ee2
rahul-797 <rahulsangwan797@gmail.com>
2024-10-24 18:36:02 +0530

    merge sorted LL

--- sortLL.cpp
+++ sortLL.cpp
@@ -13,6 +13,15 @@ struct Node {
 typedef struct Node node;
 typedef struct Node* link;
 
+void print(link head) {
+  if (head == NULL) return;
+  while (head != NULL) {
+    printf("%d ", head->data);
+    head = head->next;
+  }
+  printf("\n");
+}
+
 link createList(int* arr, int n) {
   if (n == 0) return NULL;
   link head;
@@ -52,22 +61,46 @@ link createList(int* arr, int n) {
   return head;
 }
 
-void print(link head) {
-  if (head == NULL) return;
-  while (head != NULL) {
-    printf("%d ", head->data);
-    head = head->next;
+link merge(link head1, link head2) {
+  if (head1 == NULL && head2 == NULL) return NULL;
+  if (head1 == NULL && head2 != NULL) return head2;
+  if (head2 == NULL && head1 != NULL) return head1;
+  if (head1->data > head2->data) {
+    link temp = head1;
+    head1 = head2;
+    head2 = temp;
   }
-  printf("\n");
+  link prev = head1, curr = head1->next;
+  link next2 = head2->next;
+  while (curr != NULL && head2 != NULL) {
+    if (head2->data < curr->data) {
+      head2->next = curr;
+      prev->next = head2;
+      prev = head2;
+      head2 = next2;
+      if (next2 == NULL) break;
+      next2 = next2->next;
+    } else {
+      if (curr == NULL) break;
+      prev = curr;
+      curr = curr->next;
+    }
+  }
+  if (curr == NULL) {
+    prev->next = head2;
+  }
+  return head1;
 }
 
 int main() {
   int arr1[] = {2, 6, 4, 0, 8};
-  int arr2[] = {3, 9, 1, 7, 5};
+  int arr2[] = {1, 7, 3, 5, 9};
   link head1 = createList(arr1, sizeof(arr1) / sizeof(int));
   print(head1);
   link head2 = createList(arr2, sizeof(arr2) / sizeof(int));
   print(head2);
+  link head = merge(head1, head2);
+  print(head);
   cout << endl;
   return 0;
 }
